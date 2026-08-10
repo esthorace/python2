c81b673ba644431a2d62d07f5a0b7ecf3358e8d7
Esteban <esteban.horacio.acevedo.gmail.com>
2026-08-10 20:28:23 -0300

    clase 4 wip

--- clase4/7_ejercicio.py
+++ clase4/7_ejercicio.py
@@ -33,3 +33,53 @@ def main():
 
 main()
 """
+
+
+class Usuario:
+    def __init__(self, nombre: str, contraseña: str) -> None:
+        self.__nombre = nombre
+        self.__contraseña = contraseña
+
+    def __str__(self) -> str:
+        return f"Nombre: {self.__nombre}, Contraseña: {self.__contraseña}"
+
+    @property
+    def nombre(self) -> str:
+        return self.__nombre
+
+    @nombre.getter
+    def nombre(self) -> str:
+        return self.__nombre
+
+    @nombre.setter
+    def nombre(self, nuevo_nombre: str) -> None:
+        if len(nuevo_nombre) > 4:
+            self.__nombre = nuevo_nombre
+        else:
+            print("El nombre debe tener al menos 4 caracteres")
+
+    @property
+    def contraseña(self) -> str:
+        return f"{self.__contraseña}"
+
+    @contraseña.getter
+    def contraseña(self) -> str:
+        return self.__contraseña
+
+    @contraseña.setter
+    def contraseña(self, nueva_contraseña: str) -> None:
+        if len(nueva_contraseña) > 4:
+            self.__contraseña = nueva_contraseña
+        else:
+            print("La contraseña debe tener al menos 4 caracteres")
+
+
+def main():
+    usuario = Usuario("Juan", "123456")
+    usuario.nombre = input("Ingrese el nuevo nombre: ")
+    print(usuario.nombre)
+    usuario.contraseña = input("Ingrese la nueva contraseña: ")
+    print(usuario.contraseña)
+
+
+main()
